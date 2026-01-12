b34860044b9a57498cead417b49437ce2b3802a5
mfonisoebong <mfonisoischris@gmail.com>
2026-01-12 20:41:39 +0100

    fixes

--- components/common/footer/copyright.tsx
+++ components/common/footer/copyright.tsx
@@ -2,14 +2,13 @@
 
 import Link from "next/link";
 import { FC } from "react";
-import { env } from "@/lib/env";
 
 export const Copyright: FC = () => {
   const year = new Date().getFullYear();
   return (
     <div className={"flex flex-col md:flex-row justify-between"}>
       <p className={"text-xs md:text-sm text-white"}>
-        {year}&copy; {env.app.name}. All rights reserved.
+        {year}&copy; Ripple. All rights reserved.
       </p>
       <div className="flex gap-4 text-white">
         <Link className={"text-xs md:text-sm"} href={"/"}>
